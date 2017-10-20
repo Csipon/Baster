8397413a092f763a0f7073911a60adf01782edea
AndriiSmeet <Andryuha955@gmail.com>
2017-10-20 11:53:54 +0200

    add - menu screen

--- core/src/com/team/baster/BasterGame.java
+++ core/src/com/team/baster/BasterGame.java
@@ -1,33 +1,36 @@
 package com.team.baster;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.team.baster.screen.menu.MenuScreen;
 
-public class BasterGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class BasterGame extends Game {
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create() {
+		setScreen(new MenuScreen());
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+//	SpriteBatch batch;
+//	Texture img;
+//
+//	@Override
+//	public void create () {
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
+//	}
+//
+//	@Override
+//	public void render () {
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+//	}
+//
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+//	}
 }
---
+++ core/src/com/team/baster/screen/menu/MenuScreen.java
@@ -0,0 +1,131 @@
+package com.team.baster.screen.menu;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by Smeet on 20.10.2017.
+ */
+
+public class MenuScreen implements Screen {
+
+
+    private SpriteBatch batch;
+    protected Stage stage;
+    private Viewport viewport;
+    private OrthographicCamera camera;
+    private TextureAtlas atlas;
+    protected Skin skin;
+    private static final int WORLD_WIDTH = 800;
+    private static final int WORLD_HEIGHT = 480;
+
+    public MenuScreen()
+    {
+        atlas = new TextureAtlas(Gdx.files.internal("skin/freezing-ui.atlas"));
+        skin = new Skin(Gdx.files.internal("skin/freezing-ui.json"), atlas);
+
+
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        viewport =  new ExtendViewport(WORLD_WIDTH , WORLD_HEIGHT, camera);
+        viewport.apply();
+
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        stage = new Stage(viewport, batch);
+    }
+
+
+    @Override
+    public void show() {
+
+        Gdx.input.setInputProcessor(stage);
+
+        //Create Table
+        Table mainTable = new Table();
+        //Set table to fill stage
+        mainTable.setFillParent(true);
+
+        mainTable.top();
+
+        //Create buttons
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton optionsButton = new TextButton("Options", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        //Add listeners to buttons
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+//                ((Game)Gdx.app.getApplicationListener()).setScreen(new PlayScreen());
+            }
+        });
+        exitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        //Add buttons to table
+        mainTable.add(playButton);
+        mainTable.row();
+        mainTable.add(optionsButton);
+        mainTable.row();
+        mainTable.add(exitButton);
+
+        //Add table to stage
+        stage.addActor(mainTable);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        skin.dispose();
+        atlas.dispose();
+    }
+}
+
