2d4ba264e2f7c8c1d2331ca5d6516608e904fd71
dmitriychalienko <dmitriy.chalienko@gmail.com>
2017-11-09 14:06:55 +0200

    #7 - Added getTopX scores from back-end

--- android/src/com/team/baster/service/impl/ScoreServiceImpl.java
+++ android/src/com/team/baster/service/impl/ScoreServiceImpl.java
@@ -2,6 +2,7 @@ package com.team.baster.service.impl;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.RequestFuture;
 import com.badlogic.gdx.utils.Array;
 import com.team.baster.service.ScoreService;
 import com.team.baster.storage.ScoreStorage;
@@ -9,11 +10,18 @@ import com.team.baster.storage.UserStorage;
 import com.team.baster.storage.model.Score;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import util.RequestUtil;
 
@@ -54,20 +62,13 @@ public final class ScoreServiceImpl implements ScoreService{
     //todo
     @Override
     public List<Score> getTopX(int x){
+        RequestFuture<JSONArray> future = RequestFuture.newFuture();
         try {
             RequestUtil.instance.getArray(new URL(SERVER_URL + "/score/getTopX?x=" + x),
-                    new Response.Listener<JSONArray>() {
-                        @Override
-                        public void onResponse(JSONArray response) {
-                            System.out.println(response);
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            System.err.print(error);
-                        }
-                    }, null);
-        } catch (MalformedURLException e) {
+                    future, future, null);
+            JSONArray array = future.get(1, TimeUnit.SECONDS);
+            return parseJSONArrayToList(array);
+        } catch (MalformedURLException | InterruptedException | ExecutionException | TimeoutException | JSONException e) {
             e.printStackTrace();
         }
         return null;
@@ -88,5 +89,20 @@ public final class ScoreServiceImpl implements ScoreService{
         return scoreStorage.readLastBestScore();
     }
 
-
+    private static List<Score> parseJSONArrayToList(JSONArray array) throws JSONException {
+        if (array != null) {
+            List<Score> scores = new ArrayList<>();
+            int len = array.length();
+            for (int i =0; i < len; i++) {
+                Score score = new Score();
+                JSONObject object = array.getJSONObject(i);
+                score.setScore(object.getInt("score"));
+                score.setLogin(object.getString("login"));
+                score.setDate(new Date(object.getLong("date")));
+                scores.add(score);
+            }
+            return scores;
+        }
+        return Collections.emptyList();
+    }
 }
